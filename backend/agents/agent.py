7e5c66f468c7b9b5ba422809ea24d0a9d915541d
Sanjay <sanjaybalnad@gmail.com>
2026-05-15 11:24:02 +0530

    feat: Update dependencies and enhance agent functionality

--- backend/agents/agent.py
+++ backend/agents/agent.py
@@ -1,12 +1,17 @@
 from langchain.agents import create_agent
+from langchain.agents.structured_output import ToolStrategy
 from agents.prompts import SYSTEM_PROMPT
+from schemas.schema import ModelOutput
 import config
 
 _agent = create_agent(
-        model=config.MODEL,
-        tools=[],
-        system_prompt=SYSTEM_PROMPT
+        model=config.MODEL, # Choose google model
+        # tools=[], # In our case no need of tool 
+        system_prompt=SYSTEM_PROMPT, # Sys Prompt to the model
+        response_format=ToolStrategy(ModelOutput) # Output format for the model
 )
 
 def getAgent():
     return _agent
+
+
---
+++ backend/schemas/schema.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+# TODO: Research on google drive api and write structured output for model
+class ModelOutput(BaseModel):
+    pass
